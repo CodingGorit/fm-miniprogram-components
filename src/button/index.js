a645079656d583a827d32e81b78561a80d4004a3
CodingGorit <1225277469@qq.com>
2022-05-15 16:09:18 +0800

    test to publish npm, and work success

---
+++ src/button/index.js
@@ -0,0 +1,26 @@
+Component({
+  behaviors: [],
+  properties: {
+    msg: String
+  },
+  data: {
+    msg: '123'
+  },
+  lifetimes: {
+    created() {
+
+    },
+    attached() {
+
+    },
+    moved() {
+
+    },
+    detached() {
+
+    },
+  },
+  methods: {
+
+  },
+});
